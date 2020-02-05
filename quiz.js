6da6fa9b1a5a08dd85d2bdc4808935cf95359b91
pbemerk <pbemerk@gmail.com>
2020-02-05 01:21:08 -0500

    added clue for incorrect guess

--- quiz.js
+++ quiz.js
@@ -1,8 +1,21 @@
+var correctGuess = false;
 var randomNumber = Math.floor(Math.random() * 20) + 1;
 var guess = prompt("I'm thinking of a number between 1 and 20. What is it?")
 
 if (parseInt(guess) === randomNumber ) {
-    document.write("<p>You guessed the number!</p>");
+    correctGuess = true;
+} 
+
+if ( correctGuess ) {
+    document.write('<p>You guessed the number!</p>');
+} else if ( guess > randomNumber ) {
+    prompt("My number is lower, guess again!");
+} else if ( guess < randomNumber) {
+    prompt("My number is higher, guess again!");
+}
+
+if ( correctGuess ) {
+    document.write('<p>You guessed the number!</p>');
 } else {
     document.write("<p>Sorry. The number was " + randomNumber + ".</p>");
 }
