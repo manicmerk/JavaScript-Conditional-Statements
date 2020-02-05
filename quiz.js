8aef0cb40f9dde9a2d24e5e295334ba89813b6d3
pbemerk <pbemerk@gmail.com>
2020-02-05 02:06:28 -0500

    think I finally got it sorted correctly?

--- quiz.js
+++ quiz.js
@@ -1,21 +1,22 @@
 var correctGuess = false;
 var randomNumber = Math.floor(Math.random() * 20) + 1;
 var guess = prompt("I'm thinking of a number between 1 and 20. What is it?")
+console.log(randomNumber);
 
 if (parseInt(guess) === randomNumber ) {
     correctGuess = true;
-} 
-
-if ( correctGuess ) {
-    document.write('<p>You guessed the number!</p>');
-} else if ( guess > randomNumber ) {
-    prompt("My number is lower, guess again!");
-} else if ( guess < randomNumber) {
-    prompt("My number is higher, guess again!");
+} else if ( parseInt(guess) > randomNumber) {
+    var guessLow = prompt("Your guess was too high, my number is lower than " + guess + ".");
+}    if ( parseInt(guessLow) === randomNumber) {
+    correctGuess = true;
+} else if ( parseInt(guess) < randomNumber) {
+    var guessHigh = prompt("Your guess was too low, my number is higher than " + guess + ".");
+    if ( parseInt(guessHigh) === randomNumber)
+    correctGuess = true;
 }
 
-if ( correctGuess ) {
-    document.write('<p>You guessed the number!</p>');
+if (correctGuess) {
+    document.write("<p>You guessed the number!</p>");
 } else {
-    document.write("<p>Sorry. The number was " + randomNumber + ".</p>");
+    document.write("<p>Sorry, that's not correct. The number was " + randomNumber + "</p>");
 }
