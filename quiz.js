fa12fb64c933fc73aa232febb030357fe54f98cd
pbemerk <pbemerk@gmail.com>
2020-02-05 02:16:16 -0500

    added documentation to JS

--- quiz.js
+++ quiz.js
@@ -1,7 +1,26 @@
+/*
+The Random Number Guessing Game 
+Generates a number between 1 and 20
+and gives players 2 attempts with a clue
+to guess the number correctly
+*/
+
+// assume the player didn't guess correctly
 var correctGuess = false;
+
+// generate a number from 1 - 20
 var randomNumber = Math.floor(Math.random() * 20) + 1;
+
 var guess = prompt("I'm thinking of a number between 1 and 20. What is it?")
+
+// added to improve my ability to debug
 console.log(randomNumber);
+
+/* test to see if player is
+1. correct
+2. guessed to low
+3. guessed to high
+*/
 
 if (parseInt(guess) === randomNumber ) {
     correctGuess = true;
@@ -15,6 +34,7 @@ if (parseInt(guess) === randomNumber ) {
     correctGuess = true;
 }
 
+// test if player is correct or not and display proper message
 if (correctGuess) {
     document.write("<p>You guessed the number!</p>");
 } else {
